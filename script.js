3da9259f9e36efa209c8036eff0c4a6d85c1884e
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-06-25 01:46:46 +0300

    feat: add XHR AJAX call to REST Countries API

--- script.js
+++ script.js
@@ -3,10 +3,40 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-// NEW COUNTRIES API URL (use instead of the URL shown in videos):
-// https://restcountries.com/v2/name/portugal
-
-// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+
+  request.open(
+    'GET',
+    `https://api.restcountries.com/countries/v5/names.common/${country}`,
+  );
+  request.setRequestHeader('Authorization', `Bearer ${CONFIG.API_KEY}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText).data.objects[0];
+    console.log(data);
+
+    const html = `
+<article class="country">
+          <img class="country__img" src="${data.flag.url_png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.names.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} Million</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>
+`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('united states');
+getCountryData('kenya');
